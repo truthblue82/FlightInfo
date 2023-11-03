08e35ae2c6f00fb23a7472f57a51027586cec005
Yen Truong <truthblue82@gmail.com>
2023-11-03 15:54:28 -0500

    update code

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   //APP_BASE_URL: "https://flight-info-monster.web.app",
   //REST_API_SERVICE: "https://flight-info-smonter-bec43e8ec87e.herokuapp.com",
+
   APP_BASE_URL: "http://localhost:4200",
   REST_API_SERVICE: "http://localhost:3000",
-  //GOOGLE_CLIENT_ID: "302919220885-m5gdbbjdfj6jjd1i47o6asaqmp9se5hu.apps.googleusercontent.com"
-  GOOGLE_CLIENT_ID: "302919220885-5rinvg7hctlibs1gagthlts8sg1c90o7.apps.googleusercontent.com"
+  //GOOGLE_CLIENT_ID: ""
 };
